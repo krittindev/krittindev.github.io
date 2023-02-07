2393b6607ed32772a94d5ca4876b59913d0e41ae
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-07 17:28:34 +0700

    ci: init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'tuliptgr.github.io' : '',
 }
 
 module.exports = nextConfig
