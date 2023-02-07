9992542f5cdcf159e9978b7c8e949e03ec1bd004
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-07 11:54:00 +0700

    ci: fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
 
-module.exports = nextConfig
+const repo = 'tuliptgr.github.io'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
