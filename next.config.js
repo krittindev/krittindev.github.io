16defacea34b79b8833d2840671b89bacb6b22e1
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-07 13:32:07 +0700

    project initialization

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+const nextConfig = {
+  reactStrictMode: true,
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  experimental: {
-    appDir: true,
-  },
-}
+module.exports = nextConfig
