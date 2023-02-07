c310a6dbab1657f63e2106613258682f1eb8cfe6
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-07 12:08:39 +0700

    ci: config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,14 @@ let assetPrefix = ''
 let basePath = '/'
 
 if (isGithubActions) {
+  // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
   basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
