a4797a56098ec3ed62b423c2a83738e4c0d5cb1b
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-19 11:24:14 +0700

    fix: next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'krittin.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
